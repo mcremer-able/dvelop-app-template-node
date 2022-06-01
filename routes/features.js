3fbef704cf39864e72d2b96f25c5a178693e9a00
Desimir Dimitrov <desimir.dimitrov@wesystems.ag>
2022-06-01 14:32:36 +0300

    Add dmsobjectextentions file

--- routes/features.js
+++ routes/features.js
@@ -1,35 +1,34 @@
-const express = require('express');
+const express = require("express");
 
 module.exports = function (assetBasePath, basePath) {
-    const router = express.Router();
+  const router = express.Router();
 
-    // ATTENTION: This page does not use the authenticate middleware meaning its publicly available
+  // ATTENTION: This page does not use the authenticate middleware meaning its publicly available
 
-    router.get('/', function (req, res, next) {
-        res.format({
-            'application/hal+json': function () {
-                res.send(
-                    {
-                        features: [
-                            {
-                                Url: `${basePath}/vacationrequest/`,
-                                Title: 'Vacation management',
-                                Subtitle: 'Manage vacation requests',
-                                IconURI: `${assetBasePath}/feature_icon.svg`,
-                                Summary: 'Your vacation requests and the requests of your employees',
-                                Description: 'Apply for vacation and approve the vacation requests of your employees',
-                                Color: '#adff2f',
-
-                            }
-                        ]
-                    }
-                )
+  router.get("/", function (req, res, next) {
+    res.format({
+      "application/hal+json": function () {
+        res.send({
+          features: [
+            {
+              Url: `${basePath}/vacationrequest/`,
+              Title: "Vacation management",
+              Subtitle: "Manage vacation requests",
+              IconURI: `${assetBasePath}/feature_icon.svg`,
+              Summary:
+                "Your vacation requests and the requests of your employees",
+              Description:
+                "Apply for vacation and approve the vacation requests of your employees",
+              Color: "#adff2f",
             },
-
-            'default': function () {
-                res.status(406).send('Not Acceptable')
-            }
+          ],
         });
+      },
+
+      default: function () {
+        res.status(406).send("Not Acceptable");
+      },
     });
-    return router;
+  });
+  return router;
 };
--- routes/root.js
+++ routes/root.js
@@ -1,35 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 module.exports = function (assetBasePath, basePath, version) {
   const router = express.Router();
 
   // ATTENTION: This page does not use the authenticate middleware meaning its publicly available
 
-  router.get('/', function (req, res, next) {
+  router.get("/", function (req, res, next) {
     res.format({
-      'text/html': function () {
-        res.render('root', {
-          title: 'Root of your app',
+      "text/html": function () {
+        res.render("root", {
+          title: "Root of your app",
           stylesheet: `${assetBasePath}/root.css`,
-          version: version
+          version: version,
         });
       },
-      'application/hal+json': function () {
-        res.send(
-          {
-            _links: {
-              featuresdescription: {
-                href: `${basePath}/features`
-              }
-            }
-          }
-        )
+      "application/hal+json": function () {
+        res.send({
+          _links: {
+            featuresdescription: {
+              href: `${basePath}/features`,
+            },
+            dmsobjectextensions: {
+              href: `${basePath}/dmsobjectextension`,
+            },
+          },
+        });
       },
-      'default': function () {
-        res.status(406).send('Not Acceptable')
-      }
+      default: function () {
+        res.status(406).send("Not Acceptable");
+      },
     });
   });
   return router;
 };
-
