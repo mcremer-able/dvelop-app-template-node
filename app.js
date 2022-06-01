56c5a1f07ef9745b2658e0cdd253f6d8b0e93183
Desimir Dimitrov <desimir.dimitrov@wesystems.ag>
2022-06-01 14:43:41 +0300

    Add routert dmsobjectextentions

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const dvelop = require('@dvelop-sdk/express-utils');
-require('dotenv').config()
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const dvelop = require("@dvelop-sdk/express-utils");
+require("dotenv").config();
 
-const appName = 'hackathon-demo';
+const appName = "hackathon-demo";
 const basePath = "/" + appName;
 const assetBasePath = process.env.ASSET_BASE_PATH || `/${appName}/assets`;
-const version = process.env.BUILD_VERSION || '1.0.1';
+const version = process.env.BUILD_VERSION || "1.0.1";
 
-const rootRouter = require('./routes/root')(assetBasePath, basePath, version);
-const cloudCenterEventRouter = require('./routes/cloudcenterevents')();
-const featuresRouter = require('./routes/features')(assetBasePath, basePath);
-const vacationrequestRouter = require('./routes/vacationrequest')(assetBasePath);
-const idpDemoRouter = require('./routes/idpdemo')(assetBasePath);
+const rootRouter = require("./routes/root")(assetBasePath, basePath, version);
+const cloudCenterEventRouter = require("./routes/cloudcenterevents")();
+const featuresRouter = require("./routes/features")(assetBasePath, basePath);
+const vacationrequestRouter = require("./routes/vacationrequest")(
+  assetBasePath
+);
+const idpDemoRouter = require("./routes/idpdemo")(assetBasePath);
+const dmsobjectextensions = require("./routes/dmsobjectextentions");
 
 const app = express();
 
@@ -25,30 +28,39 @@ app.use(cookieParser());
 app.use(express.json());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 app.locals.base = basePath;
 
-app.use(basePath + '/dvelop-cloud-lifecycle-event', dvelop.validateCloudCenterEventSignatureMiddlewareFactory('APP_SECRET'), cloudCenterEventRouter);
+app.use(
+  basePath + "/dvelop-cloud-lifecycle-event",
+  dvelop.validateCloudCenterEventSignatureMiddlewareFactory("APP_SECRET"),
+  cloudCenterEventRouter
+);
 
 app.use(dvelop.contextMiddleware);
 // app.use(dvelop.validateSignatureMiddlewareFactory('APP_SECRET')); // ATTENTION: This middleware should be commented in for every request once the app runs within d.velop context
 
-logger.token('tenantId', function getTenantId(req) {
-  return req.dvelopContext.tenantId
+logger.token("tenantId", function getTenantId(req) {
+  return req.dvelopContext.tenantId;
 });
-logger.token('requestId', function getRequestId(req) {
-  return req.dvelopContext.requestId
+logger.token("requestId", function getRequestId(req) {
+  return req.dvelopContext.requestId;
 });
-app.use(logger('[ctx@49610 rid=":requestId" tn=":tenantId"][http@49610 method=":method" url=":url" millis=":response-time" sbytes=":res[content-length]" status=":status"] '));
-app.use(express.json({ type: ['application/json', 'application/*+json'] }));
+app.use(
+  logger(
+    '[ctx@49610 rid=":requestId" tn=":tenantId"][http@49610 method=":method" url=":url" millis=":response-time" sbytes=":res[content-length]" status=":status"] '
+  )
+);
+app.use(express.json({ type: ["application/json", "application/*+json"] }));
 
-app.use(assetBasePath, express.static(path.join(__dirname, 'web')));
+app.use(assetBasePath, express.static(path.join(__dirname, "web")));
 
-app.use(basePath + '/', rootRouter);
-app.use(basePath + '/features', featuresRouter);
-app.use(basePath + '/vacationrequest/', vacationrequestRouter);
-app.use(basePath + '/idpdemo', idpDemoRouter);
+app.use(basePath + "/", rootRouter);
+app.use(basePath + "/features", featuresRouter);
+app.use(basePath + "/vacationrequest/", vacationrequestRouter);
+app.use(basePath + "/idpdemo", idpDemoRouter);
+app.use(basePath + "/dmsobjectextensions", dmsobjectextensions);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -59,13 +71,13 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   console.error(err.message);
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/dmsobjectextentions.js
+++ routes/dmsobjectextentions.js
@@ -1,42 +1,41 @@
 const express = require("express");
 
-module.exports = function (assetBasePath, basePath) {
-  const router = express.Router();
+const router = express.Router();
 
-  // ATTENTION: This page does not use the authenticate middleware meaning its publicly available
+// ATTENTION: This page does not use the authenticate middleware meaning its publicly available
 
-  router.get("/", function (req, res, next) {
-    res.format({
-      "application/hal+json": function () {
-        res.send({
-          extensions: [
-            {
-              id: "myapp.exportList",
-              activationConditions: [
-                {
-                  propertyId: "repository.id",
-                  operator: "or",
-                  values: ["e632f767-5cfa-538d-ab55-6756c36a74c9"],
-                },
-              ],
-              captions: [
-                { culture: "de", caption: "Liste exportieren" },
-                { culture: "en", caption: "Export list" },
-                { culture: "en-GB", caption: "Export list in Great Britain" },
-              ],
-              context: "DmsObjectListContextAction",
-              uriTemplate:
-                "/hackathon-demo/dosomething?files={dmsobjectlist.url}",
-              iconUri: "/hackathon-demo/images/export-list-64x64.png",
-            },
-          ],
-        });
-      },
+router.get("/", function (req, res, next) {
+  res.format({
+    "application/hal+json": function () {
+      res.send({
+        extensions: [
+          {
+            id: "myapp.exportList",
+            activationConditions: [
+              {
+                propertyId: "repository.id",
+                operator: "or",
+                values: ["e632f767-5cfa-538d-ab55-6756c36a74c9"],
+              },
+            ],
+            captions: [
+              { culture: "de", caption: "Liste exportieren" },
+              { culture: "en", caption: "Export list" },
+              { culture: "en-GB", caption: "Export list in Great Britain" },
+            ],
+            context: "DmsObjectListContextAction",
+            uriTemplate:
+              "/hackathon-demo/dosomething?files={dmsobjectlist.url}",
+            iconUri: "/hackathon-demo/images/export-list-64x64.png",
+          },
+        ],
+      });
+    },
 
-      default: function () {
-        res.status(406).send("Not Acceptable");
-      },
-    });
+    default: function () {
+      res.status(406).send("Not Acceptable");
+    },
   });
-  return router;
-};
+});
+
+module.exports = router;
