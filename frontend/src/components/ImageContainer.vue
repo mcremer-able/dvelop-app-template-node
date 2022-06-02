0f61ce33f4d4eb972623269569746c50b9ff9623
mcremer-able <marc.cremer@able-group.de>
2022-06-02 13:33:23 +0200

    frontend changes

--- frontend/src/components/ImageContainer.vue
+++ frontend/src/components/ImageContainer.vue
@@ -1,372 +1,45 @@
 <template>
-  <div>
-    <div class="fancy-card-grid">
+     <div class="fancy-card-grid">
       <a href="#" class="fancy-card">
-        <span class="fancy-card__number" aria-hidden="true">/ 1</span>
-        <h2 class="fancy-card__title">UI/UX Tools</h2>
-        <span class="fancy-card__arrow" aria-hidden="true">&#8594;</span>
+        <div class="imageandtext">
+          <span class="fancy-card__number" aria-hidden="true">{{mime}}</span>
 
-        <svg
-          class="fancy-card__image"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="harddrive"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M12 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm-7.5.5a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1zM5 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zM8 8a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-          />
-          <path
-            d="M12 7a4 4 0 0 1-3.937 4c-.537.813-1.02 1.515-1.181 1.677a1.102 1.102 0 0 1-1.56-1.559c.1-.098.396-.314.795-.588A4 4 0 0 1 8 3a4 4 0 0 1 4 4zm-1 0a3 3 0 1 0-3.891 2.865c.667-.44 1.396-.91 1.955-1.268c.224-.144.483.115.34.34l-.62.96A3.001 3.001 0 0 0 11 7z"
-          />
-          <path
-            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="monitor"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8.954 17H2.75A1.75 1.75 0 0 1 1 15.25V3.75C1 2.784 1.784 2 2.75 2h18.5c.966 0 1.75.784 1.75 1.75v11.5A1.75 1.75 0 0 1 21.25 17h-6.204c.171 1.375.805 2.652 1.769 3.757A.75.75 0 0 1 16.25 22h-8.5a.75.75 0 0 1-.565-1.243c.964-1.105 1.598-2.382 1.769-3.757zM21.5 3.75v11.5a.25.25 0 0 1-.25.25H2.75a.25.25 0 0 1-.25-.25V3.75a.25.25 0 0 1 .25-.25h18.5a.25.25 0 0 1 .25.25zM13.537 17c.125 1.266.564 2.445 1.223 3.5H9.24c.659-1.055 1.097-2.234 1.223-3.5h3.074z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tablet"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M10.25 5.25A.75.75 0 0 1 11 4.5h2A.75.75 0 0 1 13 6h-2a.75.75 0 0 1-.75-.75zM12 19.5a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-            fill="currentColor"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M4 2.75C4 1.784 4.784 1 5.75 1h12.5c.966 0 1.75.784 1.75 1.75v18.5A1.75 1.75 0 0 1 18.25 23H5.75A1.75 1.75 0 0 1 4 21.25V2.75zm1.75-.25a.25.25 0 0 0-.25.25v18.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V2.75a.25.25 0 0 0-.25-.25H5.75z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tv"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 32 32"
-        >
-          <path
-            d="M9.472 4a4.25 4.25 0 0 0-4.095 3.113L4.436 10.5L4.5 10.5h2.01l.793-2.852A2.25 2.25 0 0 1 9.472 6H22.53a2.25 2.25 0 0 1 2.168 1.648l3.219 11.588a2.18 2.18 0 0 1-2.1 2.764H14.5v2h11.317c2.764 0 4.767-2.635 4.027-5.299L26.626 7.113A4.25 4.25 0 0 0 22.531 4H9.471zM14.5 26H24a1 1 0 1 1 0 2H14.47c.02-.164.03-.33.03-.5V26zM4.5 14a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5h-6zm-2.5.5A2.5 2.5 0 0 1 4.5 12h6a2.5 2.5 0 0 1 2.5 2.5v13a2.5 2.5 0 0 1-2.5 2.5h-6A2.5 2.5 0 0 1 2 27.5v-13zM7.5 20a2 2 0 1 0 0-4a2 2 0 0 0 0 4z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="mobile devices"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M4 7c0-.55.45-1 1-1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-1.1 0-2 .9-2 2v11h-.5c-.83 0-1.5.67-1.5 1.5S.67 20 1.5 20H14v-3H4V7zm19 1h-6c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1zm-1 9h-4v-7h4v7z"
-            fill="currentColor"
-          />
-        </svg>
+          <img :src="url" alt="" />
+        </div>
       </a>
-      <!-- End Fancy Card 1 -->
-
-      <a href="#" class="fancy-card fancy-card--large fancy-card--offset-up">
-        <span class="fancy-card__number" aria-hidden="true">/ 2</span>
-        <h2 class="fancy-card__title">Device Mockups</h2>
-        <span class="fancy-card__arrow" aria-hidden="true">&#8594;</span>
-
-        <svg
-          class="fancy-card__image"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="harddrive"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M12 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm-7.5.5a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1zM5 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zM8 8a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-          />
-          <path
-            d="M12 7a4 4 0 0 1-3.937 4c-.537.813-1.02 1.515-1.181 1.677a1.102 1.102 0 0 1-1.56-1.559c.1-.098.396-.314.795-.588A4 4 0 0 1 8 3a4 4 0 0 1 4 4zm-1 0a3 3 0 1 0-3.891 2.865c.667-.44 1.396-.91 1.955-1.268c.224-.144.483.115.34.34l-.62.96A3.001 3.001 0 0 0 11 7z"
-          />
-          <path
-            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="monitor"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8.954 17H2.75A1.75 1.75 0 0 1 1 15.25V3.75C1 2.784 1.784 2 2.75 2h18.5c.966 0 1.75.784 1.75 1.75v11.5A1.75 1.75 0 0 1 21.25 17h-6.204c.171 1.375.805 2.652 1.769 3.757A.75.75 0 0 1 16.25 22h-8.5a.75.75 0 0 1-.565-1.243c.964-1.105 1.598-2.382 1.769-3.757zM21.5 3.75v11.5a.25.25 0 0 1-.25.25H2.75a.25.25 0 0 1-.25-.25V3.75a.25.25 0 0 1 .25-.25h18.5a.25.25 0 0 1 .25.25zM13.537 17c.125 1.266.564 2.445 1.223 3.5H9.24c.659-1.055 1.097-2.234 1.223-3.5h3.074z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tablet"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M10.25 5.25A.75.75 0 0 1 11 4.5h2A.75.75 0 0 1 13 6h-2a.75.75 0 0 1-.75-.75zM12 19.5a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-            fill="currentColor"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M4 2.75C4 1.784 4.784 1 5.75 1h12.5c.966 0 1.75.784 1.75 1.75v18.5A1.75 1.75 0 0 1 18.25 23H5.75A1.75 1.75 0 0 1 4 21.25V2.75zm1.75-.25a.25.25 0 0 0-.25.25v18.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V2.75a.25.25 0 0 0-.25-.25H5.75z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tv"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 32 32"
-        >
-          <path
-            d="M9.472 4a4.25 4.25 0 0 0-4.095 3.113L4.436 10.5L4.5 10.5h2.01l.793-2.852A2.25 2.25 0 0 1 9.472 6H22.53a2.25 2.25 0 0 1 2.168 1.648l3.219 11.588a2.18 2.18 0 0 1-2.1 2.764H14.5v2h11.317c2.764 0 4.767-2.635 4.027-5.299L26.626 7.113A4.25 4.25 0 0 0 22.531 4H9.471zM14.5 26H24a1 1 0 1 1 0 2H14.47c.02-.164.03-.33.03-.5V26zM4.5 14a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5h-6zm-2.5.5A2.5 2.5 0 0 1 4.5 12h6a2.5 2.5 0 0 1 2.5 2.5v13a2.5 2.5 0 0 1-2.5 2.5h-6A2.5 2.5 0 0 1 2 27.5v-13zM7.5 20a2 2 0 1 0 0-4a2 2 0 0 0 0 4z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="mobile devices"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M4 7c0-.55.45-1 1-1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-1.1 0-2 .9-2 2v11h-.5c-.83 0-1.5.67-1.5 1.5S.67 20 1.5 20H14v-3H4V7zm19 1h-6c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1zm-1 9h-4v-7h4v7z"
-            fill="currentColor"
-          />
-        </svg>
-      </a>
-      <!-- End Fancy Card 2 -->
-
-      <a href="#" class="fancy-card fancy-card--large fancy-card--offset-down">
-        <span class="fancy-card__number" aria-hidden="true">/ 3</span>
-        <h2 class="fancy-card__title">Scene Creators</h2>
-        <span class="fancy-card__arrow" aria-hidden="true">&#8594;</span>
-
-        <svg
-          class="fancy-card__image"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="harddrive"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M12 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm-7.5.5a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1zM5 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zM8 8a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-          />
-          <path
-            d="M12 7a4 4 0 0 1-3.937 4c-.537.813-1.02 1.515-1.181 1.677a1.102 1.102 0 0 1-1.56-1.559c.1-.098.396-.314.795-.588A4 4 0 0 1 8 3a4 4 0 0 1 4 4zm-1 0a3 3 0 1 0-3.891 2.865c.667-.44 1.396-.91 1.955-1.268c.224-.144.483.115.34.34l-.62.96A3.001 3.001 0 0 0 11 7z"
-          />
-          <path
-            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="monitor"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8.954 17H2.75A1.75 1.75 0 0 1 1 15.25V3.75C1 2.784 1.784 2 2.75 2h18.5c.966 0 1.75.784 1.75 1.75v11.5A1.75 1.75 0 0 1 21.25 17h-6.204c.171 1.375.805 2.652 1.769 3.757A.75.75 0 0 1 16.25 22h-8.5a.75.75 0 0 1-.565-1.243c.964-1.105 1.598-2.382 1.769-3.757zM21.5 3.75v11.5a.25.25 0 0 1-.25.25H2.75a.25.25 0 0 1-.25-.25V3.75a.25.25 0 0 1 .25-.25h18.5a.25.25 0 0 1 .25.25zM13.537 17c.125 1.266.564 2.445 1.223 3.5H9.24c.659-1.055 1.097-2.234 1.223-3.5h3.074z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tablet"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M10.25 5.25A.75.75 0 0 1 11 4.5h2A.75.75 0 0 1 13 6h-2a.75.75 0 0 1-.75-.75zM12 19.5a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-            fill="currentColor"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M4 2.75C4 1.784 4.784 1 5.75 1h12.5c.966 0 1.75.784 1.75 1.75v18.5A1.75 1.75 0 0 1 18.25 23H5.75A1.75 1.75 0 0 1 4 21.25V2.75zm1.75-.25a.25.25 0 0 0-.25.25v18.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V2.75a.25.25 0 0 0-.25-.25H5.75z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tv"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 32 32"
-        >
-          <path
-            d="M9.472 4a4.25 4.25 0 0 0-4.095 3.113L4.436 10.5L4.5 10.5h2.01l.793-2.852A2.25 2.25 0 0 1 9.472 6H22.53a2.25 2.25 0 0 1 2.168 1.648l3.219 11.588a2.18 2.18 0 0 1-2.1 2.764H14.5v2h11.317c2.764 0 4.767-2.635 4.027-5.299L26.626 7.113A4.25 4.25 0 0 0 22.531 4H9.471zM14.5 26H24a1 1 0 1 1 0 2H14.47c.02-.164.03-.33.03-.5V26zM4.5 14a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5h-6zm-2.5.5A2.5 2.5 0 0 1 4.5 12h6a2.5 2.5 0 0 1 2.5 2.5v13a2.5 2.5 0 0 1-2.5 2.5h-6A2.5 2.5 0 0 1 2 27.5v-13zM7.5 20a2 2 0 1 0 0-4a2 2 0 0 0 0 4z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="mobile devices"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M4 7c0-.55.45-1 1-1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-1.1 0-2 .9-2 2v11h-.5c-.83 0-1.5.67-1.5 1.5S.67 20 1.5 20H14v-3H4V7zm19 1h-6c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1zm-1 9h-4v-7h4v7z"
-            fill="currentColor"
-          />
-        </svg> </a
-      ><!-- End Fancy Card 3 -->
-
-      <a href="#" class="fancy-card">
-        <span class="fancy-card__number" aria-hidden="true">/ 4</span>
-        <h2 class="fancy-card__title">Branding Mockups</h2>
-        <span class="fancy-card__arrow" aria-hidden="true">&#8594;</span>
-
-        <svg
-          class="fancy-card__image"
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="harddrive"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M12 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm0 11a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zm-7.5.5a.5.5 0 1 0 0-1a.5.5 0 0 0 0 1zM5 2.5a.5.5 0 1 1-1 0a.5.5 0 0 1 1 0zM8 8a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-          />
-          <path
-            d="M12 7a4 4 0 0 1-3.937 4c-.537.813-1.02 1.515-1.181 1.677a1.102 1.102 0 0 1-1.56-1.559c.1-.098.396-.314.795-.588A4 4 0 0 1 8 3a4 4 0 0 1 4 4zm-1 0a3 3 0 1 0-3.891 2.865c.667-.44 1.396-.91 1.955-1.268c.224-.144.483.115.34.34l-.62.96A3.001 3.001 0 0 0 11 7z"
-          />
-          <path
-            d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H4z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          aria-labelledby="monitor"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M8.954 17H2.75A1.75 1.75 0 0 1 1 15.25V3.75C1 2.784 1.784 2 2.75 2h18.5c.966 0 1.75.784 1.75 1.75v11.5A1.75 1.75 0 0 1 21.25 17h-6.204c.171 1.375.805 2.652 1.769 3.757A.75.75 0 0 1 16.25 22h-8.5a.75.75 0 0 1-.565-1.243c.964-1.105 1.598-2.382 1.769-3.757zM21.5 3.75v11.5a.25.25 0 0 1-.25.25H2.75a.25.25 0 0 1-.25-.25V3.75a.25.25 0 0 1 .25-.25h18.5a.25.25 0 0 1 .25.25zM13.537 17c.125 1.266.564 2.445 1.223 3.5H9.24c.659-1.055 1.097-2.234 1.223-3.5h3.074z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tablet"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M10.25 5.25A.75.75 0 0 1 11 4.5h2A.75.75 0 0 1 13 6h-2a.75.75 0 0 1-.75-.75zM12 19.5a1 1 0 1 0 0-2a1 1 0 0 0 0 2z"
-            fill="currentColor"
-          />
-          <path
-            fill-rule="evenodd"
-            d="M4 2.75C4 1.784 4.784 1 5.75 1h12.5c.966 0 1.75.784 1.75 1.75v18.5A1.75 1.75 0 0 1 18.25 23H5.75A1.75 1.75 0 0 1 4 21.25V2.75zm1.75-.25a.25.25 0 0 0-.25.25v18.5c0 .138.112.25.25.25h12.5a.25.25 0 0 0 .25-.25V2.75a.25.25 0 0 0-.25-.25H5.75z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="tv"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 32 32"
-        >
-          <path
-            d="M9.472 4a4.25 4.25 0 0 0-4.095 3.113L4.436 10.5L4.5 10.5h2.01l.793-2.852A2.25 2.25 0 0 1 9.472 6H22.53a2.25 2.25 0 0 1 2.168 1.648l3.219 11.588a2.18 2.18 0 0 1-2.1 2.764H14.5v2h11.317c2.764 0 4.767-2.635 4.027-5.299L26.626 7.113A4.25 4.25 0 0 0 22.531 4H9.471zM14.5 26H24a1 1 0 1 1 0 2H14.47c.02-.164.03-.33.03-.5V26zM4.5 14a.5.5 0 0 0-.5.5v13a.5.5 0 0 0 .5.5h6a.5.5 0 0 0 .5-.5v-13a.5.5 0 0 0-.5-.5h-6zm-2.5.5A2.5 2.5 0 0 1 4.5 12h6a2.5 2.5 0 0 1 2.5 2.5v13a2.5 2.5 0 0 1-2.5 2.5h-6A2.5 2.5 0 0 1 2 27.5v-13zM7.5 20a2 2 0 1 0 0-4a2 2 0 0 0 0 4z"
-            fill="currentColor"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          role="img"
-          labelledby="mobile devices"
-          width="4em"
-          height="4em"
-          preserveAspectRatio="xMidYMid meet"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M4 7c0-.55.45-1 1-1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-1.1 0-2 .9-2 2v11h-.5c-.83 0-1.5.67-1.5 1.5S.67 20 1.5 20H14v-3H4V7zm19 1h-6c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1zm-1 9h-4v-7h4v7z"
-            fill="currentColor"
-          />
-        </svg>
-      </a>
-      <!-- End Fancy Card 4 -->
     </div>
-  </div>
 </template>
 
 <script>
-export default {};
+export default {
+    props:["url","mime"]
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 /* ------------------ */
 /* Custom Properties  */
 /* ------------------ */
+
+.fancy-card__number{
+    position: absolute;
+    top:0;
+    right:0;
+}
+
+.imageandtext {
+  position: relative;
+  margin-inline-end: auto;
+  margin-inline-start: auto;  
+  z-index: -1;
+  & > span{
+      padding-inline-end: 0.5em;
+  }
+}
+
+img {
+  max-width: min(100% - 3rem, 66rem);
+}
 
 :root {
   --clr-neutral-900: hsl(0, 0%, 9%);
@@ -459,11 +132,6 @@ select {
 /* Base Styles  */
 /* ------------------ */
 
-body {
-  background: var(--clr-neutral-100);
-  font-family: "Roboto", sans-serif;
-  line-height: 1.5;
-}
 
 h1,
 h2,
@@ -478,24 +146,20 @@ h3 {
 .fancy-card-grid,
 .credits {
   /* just for demo */
-  margin: 6rem auto;
+ // margin: 6rem auto;
   width: min(100% - 2rem, 70rem);
 }
 
-.fancy-card-grid {
-  display: grid;
-  gap: 1rem;
-  grid-template-columns: repeat(4, 1fr);
-}
+
 
 .fancy-card {
   text-decoration: none;
   color: inherit;
-  padding: 1rem;
+  padding: 0.4rem;
   display: grid;
   border: 1px solid var(--clr-neutral-400);
   border-bottom: 5px solid var(--clr-primary);
-  aspect-ratio: 1 / 0.7;
+  aspect-ratio: 1 / 1;
   grid-row: 2;
 
   &__number {
@@ -528,6 +192,7 @@ h3 {
     background: var(--clr-neutral-900);
     transform: scale(1.35);
     border: 0;
+    z-index: 99999;
 
     .fancy-card__title {
       color: var(--clr-neutral-100);
